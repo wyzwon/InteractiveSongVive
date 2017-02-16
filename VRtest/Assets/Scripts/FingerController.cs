ab146f4a39cef3b3066c12fee3f9f77de772bd4c
Thomas Bouffard <tjb5203@rit.edu>
2017-02-16 16:47:09 -0500

    -removed redundant materials *started experimenting with scripts on controllers

---
+++ VRtest/Assets/Scripts/FingerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerControler : MonoBehaviour {
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
+    private SteamVR_TrackedObject trackedObject;
+
+    // Use this for initialization
+    void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
