1c4e20b4de968532acef55c8afe5024c22ac5af1
Justin John <jxj7701@rit.edu>
2017-02-16 13:35:19 -0500

    controller work/interactivity
    Added basic controller set up for both controllers, hooked up rigid body
    for right controller and connected to Cube gameObject

---
+++ VRtest/Assets/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        if (device.GetAxis().x != 0 || device.GetAxis().y != 0)
+        {
+            print(device.GetAxis().x +  ", " + device.GetAxis().y);
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            print("trigger pressed");
+            device.TriggerHapticPulse(700);
+        }
+	}
+}
