fd88dd0061237a4808a4ecc58ab5e98dc97fcd1f
Thomas Bouffard <tjb5203@rit.edu>
2017-03-09 16:43:55 -0500

    +added Justin's sound code to the controller sound script and hooked it all up to the xzylophones

--- VRtest/Assets/Scripts/testCollideBall.cs
+++ VRtest/Assets/Scripts/testCollideBall.cs
@@ -10,6 +10,15 @@ public class testCollideBall : MonoBehaviour {
     public Material softMat;
     public Material defaultMat;
 
+    public AudioClip sound1;
+    public AudioClip sound2;
+    public AudioClip sound3;
+    public AudioClip sound4;
+    public AudioClip sound5;
+    public AudioClip sound6;
+    public AudioClip sound7;
+    public AudioClip sound8;
+
     public int HardEnough;
 
     //audio
@@ -48,6 +57,82 @@ public class testCollideBall : MonoBehaviour {
                 innerTube.GetComponent<Renderer>().material = softMat;
             }
         }//if you hit the tube
+
+
+        else if (col.gameObject.tag == "bar1")
+        {
+            print("hit");
+            GetComponent<AudioSource>().clip = sound1;
+            //print(col.relativeVelocity.magnitude);
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }//if hit hard enough
+        }//if you hit the tube
+        else if (col.gameObject.tag == "bar2")
+        {
+            GetComponent<AudioSource>().clip = sound2;
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else if (col.gameObject.tag == "bar3")
+        {
+            GetComponent<AudioSource>().clip = sound3;
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else if (col.gameObject.tag == "bar4")
+        {
+            GetComponent<AudioSource>().clip = sound4;
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else if (col.gameObject.tag == "bar5")
+        {
+            GetComponent<AudioSource>().clip = sound5;
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else if (col.gameObject.tag == "bar6")
+        {
+            GetComponent<AudioSource>().clip = sound6;
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else if (col.gameObject.tag == "bar7")
+        {
+            GetComponent<AudioSource>().clip = sound7;
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else if (col.gameObject.tag == "bar8")
+        {
+            GetComponent<AudioSource>().clip = sound8;
+            if (col.relativeVelocity.magnitude >= HardEnough)
+            {
+                //audio
+                GetComponent<AudioSource>().Play();
+            }
+        }
     }
 
     private void OnCollisionExit(Collision col)
